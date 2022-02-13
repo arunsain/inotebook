1c73a0c0c4d4901449e9847a0e623956963eb27c
arunsain3 <arunsain3@gmail.com>
2022-02-13 19:22:31 +0530

    express validator v-46 cwh

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURL = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false' ;
+const mongoURL = 'mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false' ;
 
 const connectToMongo = () =>{
 mongoose.connect(mongoURL,()=>{
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const router = express.Router();
-const User = require('../models/User')
+const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-router.post('/',(req,res) =>{
+
+router.post('/',[
+   body('name','name must be  equal or greater than 5 ').isLength({ min: 5 }),
+   body('password','password must be 5').isLength({ min: 5 }),
+   body('email','email must be  valid').isEmail(),
+],(req,res) =>{
+
+
+   const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err => {console.log(err)
+      res.json({ error:"please enter unique email" , message: err.message})
+    });
  
-const user = User(req.body);
-user.save();
-   res.send(user)
+// const user = User(req.body);
+// user.save();
+//    res.send(user)
 })
 
 module.exports = router;
