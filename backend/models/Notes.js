088a56c33a3658bf43132d02b74c200ad0251113
arunsain3 <arunsain3@gmail.com>
2022-02-15 21:12:00 +0530

    Fetching all notes & Adding a Note  v-52 cwh

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
 
 
 const NotesSchema = new Schema({
 
+	user:{
+		type : mongoose.Schema.Types.ObjectId,
+		ref:'user'
+	},
 	title : {
 		type : String,
 		required : true
@@ -13,12 +19,12 @@ const NotesSchema = new Schema({
 		required : true,
 		
 	},
-	tage : {
+	tags : {
 		type : String,
 		default : "general",
 		
 	},
-	,
+	
 	date : {
 		type : Date,
 		default : Date.now
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,14 +1,59 @@
 const express = require('express')
 const router = express.Router();
+const Notes = require("../models/Notes");
+const fetchUser = require("../middleware/fetchUser");
+const { body, validationResult } = require("express-validator");
 
 
-router.get('/',(req,res) =>{
+
+
+router.get('/fetchallnotes',fetchUser, async (req,res) =>{
+
+const notes = await	Notes.find({user:req.user.id});
 	obj = {
 
-		name : "notes data",
-		email : "arunsain3@gmail.com"
+		name :req.user.id,
+		email : notes
 	}
 	res.json(obj);
 })
+
+
+router.post(
+	"/addnotes",
+	[
+	  body("title", "title must be 5 characer").isLength({ min: 5 }),
+	  body("description", "description must be 5 characer").isLength({ min: 5 }),
+	 
+	],fetchUser,
+	async (req, res) => {
+	  // this code run if request parameter fail in validation
+	  const errors = validationResult(req);
+	  if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	  }
+  const {title,description,tags }= req.body;
+	  try {
+	
+		//  create user notes
+		const note = await Notes.create({
+			user: req.user.id,
+			title: title,
+			description: description,
+			tags: tags,
+		});
+  
+		note.save();
+  
+		
+  
+		return res.status(200).json({ note });
+	  } catch (error) {
+		console.log(error.message);
+		res.status(500).send("some error occured");
+	  }
+	}
+  );
+  
 
 module.exports = router;
