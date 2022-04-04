043c53b24ae0f0c39129ccd2fd4d007a3c27e4a6
arunsain3 <arunsain3@gmail.com>
2022-04-04 23:06:36 +0530

    setup frontend v-55 &  creating home , navbar etc pages  v-56 cwh

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>About is working</>
+  )
+}
+
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>Home is working</>
+  )
+}
+
+export default Home
